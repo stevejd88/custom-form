9513b283450a7bf76694a2a706abe081fcd6f772
stevejd88 <stevejd88@gmail.com>
2021-06-21 14:34:43 -0500

    Add checkbox and button styles

--- src/components/checkbox/checkbox.component.jsx
+++ src/components/checkbox/checkbox.component.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+import "./checkbox-styles.css";
+
 const Checkbox = ({ label, name, type, checked }) => {
   return (
-    <div>
+    <div className='check-field'>
       <input type={type} id={name} name={name} checked={checked} />
       <label for={name}>{label}</label>
     </div>
